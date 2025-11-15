913a007be56878fd34899e566d81debf95c9da55
kerthcet <kerthcet@gmail.com>
2025-11-15 11:27:54 +0000

    expose Trial
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- alphatrion/__init__.py
+++ alphatrion/__init__.py
@@ -2,15 +2,16 @@ from alphatrion.experiment.craft_exp import CraftExperiment, ExperimentConfig
 from alphatrion.log.log import log_artifact, log_metrics, log_params
 from alphatrion.runtime.runtime import init
 from alphatrion.tracing.tracing import task, workflow
-from alphatrion.trial.trial import CheckpointConfig, TrialConfig
+from alphatrion.trial.trial import CheckpointConfig, TrialConfig, Trial
 
 __all__ = [
+    "init",
     "log_artifact",
     "log_params",
     "log_metrics",
     "CraftExperiment",
     "ExperimentConfig",
-    "init",
+    "Trial",
     "TrialConfig",
     "CheckpointConfig",
     "task",
