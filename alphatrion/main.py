fe2a6fe882d4554f6593b41e4856f35ad5abdf55
Wanying Zhao <wanying.zhao54@outlook.com>
2025-11-17 20:58:06 +0000

    initialise fastAPI and mount graphql endpoint

--- alphatrion/main.py
+++ alphatrion/main.py
@@ -1,6 +1,26 @@
+from .graphql.schema import schema
+from fastapi import FastAPI
 from dotenv import load_dotenv
+from strawberry.fastapi import GraphQLRouter
 
 load_dotenv()
 
+
+app = FastAPI()
+
+# Create GraphQL router
+graphql_app = GraphQLRouter(schema)
+
+# Mount /graphql endpoint
+app.include_router(graphql_app, prefix="/graphql")
+
+
+# root endpoint for testing
+@app.get("/")
+def root():
+    return {"message": "AlphaTrion API running"}
+
+
 if __name__ == "__main__":
-    print("Hello, AlphaTrion!")
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
