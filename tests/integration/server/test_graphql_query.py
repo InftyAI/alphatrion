d4fef871f629fc47a85854c78bc519b2522c75ef
kerthcet <kerthcet@gmail.com>
2026-02-19 17:00:56 +0000

    fix test
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- tests/integration/server/test_graphql_query.py
+++ tests/integration/server/test_graphql_query.py
@@ -386,7 +386,7 @@ client = OpenAI(
 
 @tracing.workflow()
 async def create_joke():
-    completion = await client.chat.completions.create(
+    completion = client.chat.completions.create(
         model="smollm:135m",
         messages=[{"role": "user", "content": "Tell me a joke about opentelemetry"}],
     )
