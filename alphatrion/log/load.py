26f38b3b0c751757cf525c39ed2ad95964aa6d15
Kante Yin <kerthcet@gmail.com>
2026-05-23 13:41:49 +0100

    Add integration tests for s3 (#267)
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- alphatrion/log/load.py
+++ alphatrion/log/load.py
@@ -28,19 +28,22 @@ async def load_dataset(id: str | uuid.UUID, output_dir: str | None = None) -> li
     return result
 
 
+# TODO: we may need to add repo name to support sub-categorization of checkpoints,
+# e.g., "ckpt/epoch".
 async def load_checkpoint(
     id: str | uuid.UUID,
-    version: str = "latest",
+    version_or_filename: str = "latest",
     type: str = "experiment",
     output_dir: str | None = None,
 ) -> list[str]:
     """
-    Load checkpoint from artifact registry, the path is expected to be in the format of
-    "org_id/team_id/exp_id/ckpt/".
+    Load checkpoint from artifact registry, the path is expected to be in the format of:
+      - OCI: "org_id/team_id/exp_id/ckpt:version"
+      - S3: "org_id/team_id/exp_id/ckpt/filename"
 
     :param id: the id of the experiment.
-    :param version: the version of the checkpoint to load, default is "latest".
-        If version is "latest", it will load the latest version (for oci backend) or
+    :param version_or_filename: the version or filename of the checkpoint to load, default is "latest".
+        If version_or_filename is "latest", it will load the latest version (for oci backend) or
         the file with the latest timestamp (for s3 backend).
     :param type: the type of the checkpoint, can be "experiment" or "agent", default is "experiment".
     :param output_dir: the directory to which the checkpoint will be loaded.
@@ -58,15 +61,17 @@ async def load_checkpoint(
 
     # We only need to do this for s3 backend, because for oci backend,
     # the version is the tag and "latest" tag will always point to the latest version.
-    if version == "latest" and artifact.storage_type == ARTIFACT_TYPE_S3:
+    if version_or_filename == "latest" and artifact.storage_type == ARTIFACT_TYPE_S3:
         versions = artifact.list_versions(repo_name)
         if versions is None or len(versions) == 0:
             return []
 
-        version = versions[0]  # Assuming versions are sorted by time, newest first
+        version_or_filename = versions[
+            0
+        ]  # Assuming versions are sorted by time, newest first
 
     result = await asyncio.get_running_loop().run_in_executor(
-        None, artifact.pull, repo_name, version, output_dir
+        None, artifact.pull, repo_name, version_or_filename, output_dir
     )
 
     return result
